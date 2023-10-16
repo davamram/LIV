5907b9cd50a042c261a2effae954d2415407a818
David Amram <d.amram@ip2i.in2p3.fr>
2023-10-16 09:32:13 +0200

    gitlab version

--- plot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yodastack -o Results/MadGraph/Reweight/Etr_$1.yoda Results/MadGraph/Reweight/120GeV_Etr$1.yoda Results/MadGraph/Reweight/2000GeV_Etr$1.yoda Results/MadGraph/Reweight/1000GeVA_Etr$1.yoda Results/MadGraph/Reweight/1000GeVB_Etr$1.yoda
-rivet-mkhtml --errs --no-weight Results/MadGraph/Reweight/Etr_$1.yoda -o Plots/MadGraph/Reweight/$1GeV
