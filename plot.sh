a137bc0b0b00e7e14b6a2bca2f84f53c88a083a3
David Amram <d.amram@ip2i.in2p3.fr>
2025-06-20 17:30:40 +0200

    Commit last work (probably launch commands) for storage

--- plot.sh
+++ plot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yodastack -o Results/MadGraph/Reweight/Etr_$1.yoda Results/MadGraph/Reweight/120GeV_Etr$1.yoda Results/MadGraph/Reweight/2000GeV_Etr$1.yoda Results/MadGraph/Reweight/1000GeVA_Etr$1.yoda Results/MadGraph/Reweight/1000GeVB_Etr$1.yoda
-rivet-mkhtml --errs --no-weight Results/MadGraph/Reweight/Etr_$1.yoda -o Plots/MadGraph/Reweight/$1GeV
+yodastack -o Results/sherpa/Reweight/Etr_$1.yoda Results/sherpa/Reweight/120GeV_Etr$1.yoda Results/sherpa/Reweight/2000GeV_Etr$1.yoda Results/sherpa/Reweight/1000GeVA_Etr$1.yoda Results/sherpa/Reweight/1000GeVB_Etr$1.yoda
+rivet-mkhtml --errs --no-weight Results/sherpa/Reweight/Etr_$1.yoda -o Plots/sherpa/Reweight/$1GeV
